e24d7421fb78a3c9dd4ad0291706e5c54b26d587
Trin Bussmuangpak <trinbus9291@gmail.com>
2025-03-24 09:41:25 +0700

    test fetch

--- src/libs/getUserProfile.tsx
+++ src/libs/getUserProfile.tsx
@@ -1,5 +1,8 @@
+//"https://a08-venue-explorer-backend.vercel.app/api/v1/auth/me"
+//"http://localhost:5050/api/v1/auth/me"
+
 export default async function getUserProfile(token:string) {
-    const response = await fetch("https://a08-venue-explorer-backend.vercel.app/api/v1/auth/me", {
+    const response = await fetch("http://localhost:5050/api/v1/auth/me", {
         method: "GET",
         headers: {
             authorization: `Bearer ${token}`,
