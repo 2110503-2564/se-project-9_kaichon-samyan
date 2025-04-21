1184a22c40e63a9d3aec86c0b507538eff37f3c6
Chatchaphon-3 <chatchaphon2547@gmail.com>
2025-04-21 21:48:47 +0700

    fix card size ja

--- src/components/InteractiveCard.tsx
+++ src/components/InteractiveCard.tsx
@@ -14,7 +14,7 @@ export default function InteractiveCard({ children }: { children: React.ReactNod
 
     return (
         <div
-            className="w-[400px] h-[260px] rounded-3xl shadow-lg bg-white mb-5 p-4 drop-shadow-md ms-5 transition-all duration-300 ease-in-out scale-100"
+            className="w-[400px] h-[300px] rounded-3xl shadow-lg bg-white mb-5 p-4 drop-shadow-md ms-5 transition-all duration-300 ease-in-out scale-100"
             onMouseOver={onCardMouseAction}
             onMouseOut={onCardMouseAction}
         >
