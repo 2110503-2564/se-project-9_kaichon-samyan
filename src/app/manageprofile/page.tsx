f23f7b6edfa258a25a4f29c4081cf1ff066fd0a1
Teetat Juirnarongrit <butsoap316@gmail.com>
2025-04-25 11:03:18 +0700

    add pic to whoever dont have any pic

--- src/app/manageprofile/page.tsx
+++ src/app/manageprofile/page.tsx
@@ -2,12 +2,20 @@
 import getAllUsers from "@/libs/getAllUser";
 import { authOptions } from "../api/auth/[...nextauth]/authOptions";
 import { getServerSession } from "next-auth";
-import ManageProfileClient from "@/components/manageprofileclient";
+import ManageProfileClient from "@/components/ManageProfileClient";
 
 export default async function ManageProfilePage() {
   const session = await getServerSession(authOptions);
-
-  const token = session?.user?.token;
+  const token = session?.user?.token; 
+  if(session?.user.user.role !== "admin") {
+    return (
+      <div className="flex justify-center items-center mt-40">
+        <h1 className="text-5xl">
+          YOU ARE NOT ADMIN.
+        </h1>
+      </div>
+    )
+  }
 
   if (!token) {
     return <div>Error: No token found in session</div>;
